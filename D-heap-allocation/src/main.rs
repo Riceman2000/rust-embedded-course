5360efebf23595465dc818f00d395f5b6f846d0e
riceman2000 <alex4222000@gmail.com>
2025-01-27 21:04:38 -0500

    Update embed-alloc

--- D-heap-allocation/src/main.rs
+++ D-heap-allocation/src/main.rs
@@ -8,7 +8,7 @@ use alloc::vec::Vec;
 use bsp::entry;
 use defmt::info;
 use defmt_rtt as _;
-use embedded_alloc::Heap;
+use embedded_alloc::LlffHeap as Heap;
 use embedded_hal::digital::OutputPin;
 use panic_probe as _;
 
@@ -42,8 +42,8 @@ fn main() -> ! {
     {
         use core::mem::MaybeUninit;
         const HEAP_SIZE: usize = 1024;
-        static HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(&raw mut HEAP_MEM as usize, HEAP_SIZE) }
     }
 
     // Peripheral initialization
